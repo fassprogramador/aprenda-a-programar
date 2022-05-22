582740e81f89afbc51ab97e64d81906c73ec63b2
fassprogramador <105759758+fassprogramador@users.noreply.github.com>
2022-05-21 22:54:49 -0300

    Versão Final

---
+++ pratica-1.js
@@ -0,0 +1,30 @@
+
+
+
+let nome
+let email
+let profissão
+let idade
+let lugar
+
+nome = 'Francisco de Assis'
+email = 'fass@gmail.com'
+profissão = 'Engenheiro'
+idade = 43
+lugar = 'Tailandia'
+
+console.log('Nome:    ',nome,
+           'E-mail:   ',email,
+           'Profissão ',profissão,
+           'Idade:    ',idade,
+           'Lugar:    ',lugar)
+
+
+
+console.log(' Olá, ',nome,'!',' Somos a agência de turismo Viagem dos Sonhos.')
+console.log('Estamos te escrevendo este e-mail,pois acabamos de confirmar as compras')
+console.log('das passagens aéreas para',lugar,'   Confirma pra gente alguns dados?')
+console.log('A sua idade é ',idade,'você é ',profissão,'e o seu email é',email)
+console.log('Ficamos no aguardo. Muito obrigado e boa viagem! :D')
+
+
